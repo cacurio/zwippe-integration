a603755d5e6a6013c62b1959afef25680e9ff2f7
Cristhian Acurio <cristhian.acurio@outlook.com>
2023-11-30 19:27:59 -0500

    validate DTO schema

--- src/transactions/dto/transaction.dto.ts
+++ src/transactions/dto/transaction.dto.ts
@@ -1,4 +1,9 @@
-export class TransactionDTO {
-  externalId: string;
-  numberAccount: string;
-}
+import { z } from 'nestjs-zod/z';
+import { createZodDto } from 'nestjs-zod';
+
+const TransactionSchema = z.object({
+  externalId: z.string(),
+  numberAccount: z.string(),
+});
+
+export class TransactionDTO extends createZodDto(TransactionSchema) {}
